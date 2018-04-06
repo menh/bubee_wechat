db033f1dc8137cc8ae5cfb6b45e567ff1edd3c27
menhu <872204778@qq.com>
2018-04-06 15:56:35 +0800

    wenxin

--- weChat/pages/index/index.js
+++ weChat/pages/index/index.js
@@ -8,7 +8,7 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    list:[1,2,3]
+    list:[]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -17,7 +17,8 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
+    this.getdata();
+   /* if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
@@ -42,7 +43,7 @@ Page({
           })
         }
       })
-    }
+    }*/
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -51,5 +52,31 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+  getdata: function () {//定义函数名称
+    var that = this;   // 这个地方非常重要，重置data{}里数据时候setData方法的this应为以及函数的this, 如果在下方的sucess直接写this就变成了wx.request()的this了
+    wx.request({
+      url: 'http://localhost:8080/bubee/weChatGetPic.do',//请求地址
+      data: {//发送给后台的数据
+        name: "bella",
+        age: "20"
+      },
+      header: {//请求头
+        "Content-Type": "applciation/json"
+      },
+      method: "GET",//get为默认方法/POST
+      success: function (res) {
+        console.log(res.data.name);//res.data相当于ajax里面的data,为后台返回的数据
+        　　　　　　that.setData({list:res.data.list1})//如果在sucess直接写this就变成了wx.request()的this了.必须为getdata函数的this,不然无法重置调用函数
+                //that.setData({ movies: cache.movies, loading: false })
+          　　　　　//　logs: res.data.result
+          
+
+      },
+      fail: function (err) { },//请求失败
+      complete: function () { }//请求完成后执行的函数
+    })
+  },
+
+
 })
--- weChat/pages/splash/splash.js
+++ weChat/pages/splash/splash.js
@@ -1,47 +1,94 @@
-const app=getApp()
+// 获取全局应用程序实例对象
+const app = getApp()
 
+// 创建页面实例对象
 Page({
-  data:{
-    movies:[],
-    loading:true
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movies: [],
+    loading: true
   },
 
-  getCache(){
-    return new Promise(resolve=>{
+  getCache() {
+    return new Promise(resolve => {
       app.wechat.getStorage('last_splash_data')
-        .then(res=>{
-          const{movies,expires}=res.data
-          if(movies&&expires>Date.now()){
+        .then(res => {
+          const { movies, expires } = res.data
+          // 有缓存，判断是否过期
+          if (movies && expires > Date.now()) {
             return resolve(res.data)
           }
+          // 已经过期
           console.log('uncached')
           return resolve(null)
         })
-        .catch(e=>resolve(null))
+        .catch(e => resolve(null))
     })
   },
 
-  handleStart(){
+  handleStart() {
+    // TODO: 访问历史的问题
     wx.switchTab({
-      url:'../board/board'
+      url: '../board/board'
     })
   },
 
-  onLoad(){
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
     this.getCache()
-      .then(cache=>{
-        if(cache)
-          return this.setDate({movies:cache.movies,loading:false})
-      }
+      .then(cache => {
+        if (cache) {
+          return this.setData({ movies: cache.movies, loading: false })
+        }
 
-      app.douban.find('coming-soon',1,3)
-        .then(d=>{
-          this.setData({movies:d.subjects,loading:false})
-          return app.wechat.setStorage('last_splash_data',{
-            movies:d.subjects,
-            expires:Date.now()+1*24*60*60*1000
+        app.douban.find('coming_soon', 1, 3)
+          .then(d => {
+            this.setData({ movies: d.subjects, loading: false })
+            return app.wechat.setStorage('last_splash_data', {
+              movies: d.subjects,
+              expires: Date.now() + 1 * 24 * 60 * 60 * 1000
+            })
           })
-        })
-        .then(()=>console.log('storage last splash data'))
-      )}
+          .then(() => console.log('storage last splash data'))
+      })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    // TODO: onReady
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    // TODO: onShow
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+    // TODO: onHide
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+    // TODO: onUnload
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+    // TODO: onPullDownRefresh
+  }
 })
